48925632b90cb8d4a7bdc94e3beb839ddb5e36ca
pcalk <calkowska.patrycja@gmail.com>
2021-10-04 14:50:37 +0200

    04/10

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,16 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+
+    static Logger log = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+
+        log.info("message");
     }
+
+
 }
